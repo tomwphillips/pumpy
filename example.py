32aff0914e11a1d7f7d7980f5df9bcc60bff62e6
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-02-11 14:33:41 +0000

    Moved to README.md

--- example.py
+++
@@ -1,9 +0,0 @@
-import pumpy
-p11 = pumpy.Pump('/dev/tty.usbserial-FTWOFH91D',address=10,verbose=True)
-p11.setdiameter(25)
-p11.setflowrate(2000)
-p11.settargetvolume(200)
-p11.infuse()
-p11.waituntiltarget()
-p11.withdraw()
-p11.waituntiltarget()
