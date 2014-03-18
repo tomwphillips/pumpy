e48f19d0ce47e48e344356f6b63ac3a8079825f8
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2014-03-18 19:18:06 +0000

    Implemented logging module

--- pumpy.py
+++ pumpy.py
@@ -2,12 +2,9 @@ from __future__ import print_function
 import sys
 import serial
 import argparse
+import logging
 
-# Couple of convenience functions
-
-def error(*objs):
-    print("ERROR: ", *objs, file=sys.stderr)
-    sys.exit()
+logging.basicConfig(level=logging.INFO)
 
 def removecrud(string):
     # Remove trailing zeros after decimal places from a string
@@ -38,32 +35,35 @@ class Chain:
         self.verbose = verbose
         self.serialcon = serial.Serial(port = comport,stopbits = serial.STOPBITS_TWO,parity = serial.PARITY_NONE,timeout=2)
         self.clearbuffers()
-        if self.serialcon.isOpen() and verbose:
-            print('Chain:  created on',comport)
-        elif not self.serialcon.isOpen():
-            error('Chain: not created on',comport)
+        if self.serialcon.isOpen():
+            logging.info('Chain %s: created',comport)
+        else:
+            logging.critical('Chain %s: unable to create',comport)
 
     # For debugging
     def close(self):
         self.serialcon.close()
         if self.serialcon.isOpen():
-            error('Chain: ERROR - could not close serial port')
-        elif self.verbose:
-            print('Chain: serial port closed OK')
+            logging.error('Chain %s: unable to close',self.serialcon.port)
+        else:
+            logging.info('Chain %s: closed',self.serialcon.port)
 
     def clearbuffers(self):
         self.serialcon.flushOutput()
         self.serialcon.flushInput()
+        logging.info('Chain %s: flushed serial input and output buffers',self.serialcon.port)
 
 # Pump object that does everything
 class Pump:
-    def __init__(self,chain,address = 0,verbose = False):
-        self.serialcon = chain.serialcon;
+    def __init__(self,chain,address = 0, name = None):
+        self.name = name
+        self.serialcon = chain.serialcon
         self.address = '{0:02.0f}'.format(address)
-        self.verbose = verbose
         self.diameter = None
         self.flowrate = None
         self.targetvolume = None
+
+        logging.info('Pump %s: created at address %s on %s',self.name,self.address,self.serialcon.port)
 
     def __repr__(self):
         string = ''
@@ -73,15 +73,20 @@ class Pump:
 
     def setdiameter(self,diameter):
         if diameter > 35 or diameter < 0.1:
-            error('Syringe diameter must be between 0.1 and 35 mm')
+            logging.error('Pump %s: %s mm out of diameter range. Must be between 0.1-35 mm.',self.name,diameter)
+            return None
 
         diameter = str(diameter)
 
         # Pump only considers 2 d.p. - anymore are ignored
         if len(diameter) > 5:
-            diameter = diameter[0:5]
+            if diameter[2] is '.': # e.g. 30.2222222
+                diameter = diameter[0:5]
+            elif diameter[1] is '.': # e.g. 3.222222
+                diameter = diameter[0:4]
+
             diameter = removecrud(diameter)
-            print('Warning: diameter truncated to',diameter,'mm')
+            logging.warning('Pump %s: diameter truncated to %s mm',self.name,diameter)
         else:
             diameter = removecrud(diameter)
 
@@ -98,13 +103,12 @@ class Pump:
             
             # Check diameter was set accurately
             if returned_diameter != diameter:
-                error('Set diameter does not match diameter on pump.\n','Set:',diameter,'Returned:',returned_diameter)
+                logging.error('Pump %s: set diameter (%s mm) does not match diameter returned by pump (%s mm)',self.name,diameter,returned_diameter)
             elif returned_diameter == diameter:
                 self.diameter = float(returned_diameter)
-                if self.verbose:
-                     print('Diameter set to',returned_diameter,'mm')
+                logging.info('Pump %s: diameter set to %s mm',self.name,self.diameter)
         else:
-            error('Pump response not understood.')
+            logging.error('Pump %s: unknown response',self.name)
 
     def setflowrate(self,flowrate):
         flowrate = str(flowrate)
@@ -112,7 +116,7 @@ class Pump:
         if len(flowrate) > 5:
             flowrate = flowrate[0:5]
             flowrate = removecrud(flowrate)
-            print('Warning: flow rate truncated to',flowrate,'uL/min')
+            logging.warning('Pump %s: flow rate truncated to %s uL/min',self.name,flowrate)
         else:
             flowrate = removecrud(flowrate)
 
@@ -126,15 +130,14 @@ class Pump:
             returned_flowrate = removecrud(resp[2:8])
 
             if returned_flowrate != flowrate:
-                error('Set flowrate does not match flowrate on pump.\n','Set:',flowrate,'Returned:',returned_flowrate)
+                logging.error('Pump %s: set flowrate (%s uL/min) does not match flowrate returned by pump (%s uL/min)',self.name,flowrate,returned_flowrate)
             elif returned_flowrate == flowrate:
                 self.flowrate = returned_flowrate
-                if self.verbose:
-                    print('Flow rate set to',returned_flowrate,'uL/min')
+                logging.info('Pump %s: flow rate set to %s uL/min',self.name,self.flowrate)
         elif 'OOR' in resp:
-            error('Flow rate out of range')
+            logging.error('Pump %s: flow rate (%s uL/min) is out of range',self.name,flowrate)
         else:
-            error('Set flow rate: unexpected response from pump after writing flow rate')
+            logging.error('Pump %s: unknown response',self.name)
             
     def infuse(self):
         self.serialcon.write(self.address + 'RUN\r')
@@ -143,12 +146,12 @@ class Pump:
             if resp[-1] == '<': # wrong direction
                 self.serialcon.write(self.address + 'REV\r')
             else:
-                error('Infuse command: unexpected response from pump',resp)
+                logging.error('Pump %s: unexpected response to infuse',self.name)
                 break
+
             resp = self.serialcon.read(5)
 
-        if self.verbose:
-            print('Infusing')
+        logging.info('Pump %s: infusing',self.name)
 
     def withdraw(self):
         self.serialcon.write(self.address + 'REV\r')
@@ -159,20 +162,22 @@ class Pump:
                 self.serialcon.write(self.address + 'RUN\r')
             elif resp[-1] == '>': # wrong direction
                 self.serialcon.write(self.address + 'REV\r')
-            
+            else:
+                logging.error('Pump %s: unexpected response to withdraw',self.name)
+                break
+
             resp = self.serialcon.read(5)
         
-        if self.verbose:
-            print('Withdrawing')
+        logging.info('Pump %s: withdrawing',self.name)
 
     def stop(self):
         self.serialcon.write(self.address + 'STP\r')
         resp = self.serialcon.read(5)
         
         if resp[-1] != ':':
-            error('Pump not stopped',resp)
-        elif self.verbose:
-            print('Stopped')
+            logging.error('Pump %s: unexpected response to stop',self.name)
+        else:
+            logging.info('Pump %s: stopped',self.name)
 
     def settargetvolume(self,targetvolume):
         self.serialcon.write(self.address + 'MLT' + str(targetvolume) + '\r')
@@ -182,13 +187,13 @@ class Pump:
         # Pump11 replies with leading zeros, e.g. 03, but PHD2000 misbehaves and 
         # returns without and gives an extra CR. Use int() to deal with
         if int(resp[-3:-1]) != int(self.address):
-            error('Response has incorrect address')
+            logging.error('Pump %s: response has incorrect address',self.name)
         elif resp[-1] == ':' or resp[-1] == '>' or resp[-1] == '<':
             self.targetvolume = float(targetvolume)
-            if self.verbose:
-                print('Target volume set to',targetvolume,'uL')
+            logging.info('Pump %s: target volume set to %s uL',self.name,self.targetvolume)
 
     def waituntiltarget(self):
+        logging.info('Pump %s: waiting until target reached',self.name)
         # counter - need it to check if it's the first loop
         i = 0
     
@@ -198,11 +203,10 @@ class Pump:
             resp1 = self.serialcon.read(15)
 
             if ':' in resp1 and i == 0:
-                error('Pump is not infusing/withdrawing. Infuse or withdraw then run waituntiltarget().')
+                logging.error('Pump %s: not infusing/withdrawing - infuse or withdraw first',self.name)
             elif ':' in resp1 and i != 0:
                 # pump has already come to a halt
-                if self.verbose:
-                    print('Target volume reached.')
+                logging.info('Pump %s: target volume reached, stopped',self.name)
                 break
 
             # Read again
@@ -211,30 +215,29 @@ class Pump:
 
             # Check if they're the same - if they are, break, otherwise continue
             if resp1 == resp2:
-                if self.verbose:
-                    print('Target volume reached.')
+                logging.info('Pump %s: target volume reached, stopped',self.name)
                 break
 
             i = i+1
-    
+
+# PHD2000 subclass because it behaves badly    
 class PHD2000(Pump):
     def stop(self):
         self.serialcon.write(self.address + 'STP\r')
         resp = self.serialcon.read(5)
         
         if resp[-1] != '*':
-            print('-1',resp[-1])
-            error('Pump not stopped',resp)
-        elif self.verbose:
-            print('Stopped')
+            logging.error('Pump %s: unexpected response to stop',self.name)
+        else:
+            logging.info('Pump %s: stopped',self.name)
 
     def settargetvolume(self,targetvolume):    
         # PHD2000 expects target volume in mL not uL like the Pump11, so convert to mL
-        targetvolume = str(targetvolume/1000.0)
+        targetvolume = str(float(targetvolume)/1000.0)
 
         if len(targetvolume) > 5:
             targetvolume = targetvolume[0:5]
-            print('Warning: target volume truncated to',targetvolume,'uL')
+            logging.warning('Pump %s: target volume truncated to %s mL',self.name,targetvolume)
 
         self.serialcon.write(self.address + 'MLT' + targetvolume  + '\r')
         resp = self.serialcon.read(5)
@@ -243,12 +246,11 @@ class PHD2000(Pump):
         # Pump11 replies with leading zeros, e.g. 03, but PHD2000 misbehaves and 
         # returns without and gives an extra CR. Use int() to deal with
         if int(resp[-3:-1]) != int(self.address):
-            error('Response has incorrect address')
+            logging.error('Pump %s: response has incorrect address',self.name)
         elif resp[-1] == ':' or resp[-1] == '>' or resp[-1] == '<':
             # Been set correctly, so put it back in the object (as uL, not mL)
             self.targetvolume = float(targetvolume)*1000.0
-            if self.verbose:
-                print('Target volume set to',self.targetvolume,'uL')
+            logging.info('Pump %s: target volume set to %s uL',self.name,self.targetvolume)
 
 # Command line options
 # Run with -h flag to see help
@@ -258,7 +260,6 @@ if __name__ == '__main__':
     parser.add_argument('port',help='serial port')
     parser.add_argument('address',help='pump address',type=int)
     parser.add_argument('-PHD2000',help='To control PHD2000',action='store_true')
-    parser.add_argument('-v','--verbose',help='verbose mode',action='store_true')
     parser.add_argument('-d',dest='diameter',help='set syringe diameter',type=int)
     parser.add_argument('-f',dest='flowrate',help='set flow rate')
     parser.add_argument('-t',dest='targetvolume',help='set target volume')
@@ -280,9 +281,9 @@ if __name__ == '__main__':
     # 5. infuse|withdraw (+ wait for target volume)
 
     if args.PHD2000:
-        pump = PHD2000(chain,args.address,args.verbose)
+        pump = PHD2000(chain,args.address,name='PHD2000')
     else:
-        pump = Pump(chain,args.address,args.verbose)
+        pump = Pump(chain,args.address,name='11')
 
     if args.stop:
         pump.stop()
