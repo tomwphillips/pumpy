615b332e4de70127fa7596adf86622d7011a94b3
Tom Phillips <thomas.phillips07@imperial.ac.uk>
2015-06-02 10:12:55 +0100

    Fixed stupid setup.py mistake Didn't actually install anything...

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ except ImportError:
 
 config = {
     'name': 'pumpy',
-    'version': '1.1.2',
+    'version': '1.1.3',
     'description': 'Python RS-232 interface for Harvard syringe pumps',
     'url': 'https://github.com/tomwphillips/pumpy',
     'author': 'Tom W Phillips',
     'author_email': 'me@tomwphillips.co.uk',
     'license': 'MIT',
     'install_requires': ['pyserial>=2.7'],
+    'py_modules': ['pumpy']
 }
 
 setup(classifiers=[
